ad14cd700b6f13fb81b2ac409ce040490130883f
baltaMarenda <balta.marenda@gmail.com>
2025-09-20 14:21:37 -0300

    Clase modelo, retrofit y layout de item_clase.

---
+++ app/src/main/java/com/example/tp0gym/modelo/Clase.java
@@ -0,0 +1,29 @@
+package com.example.tp0gym.modelo;
+
+public class Clase {
+    private String id;
+    private String title;
+    private String description;
+    private String discipline;
+    private int defaultDurationMin;
+    private int defaultCapacity;
+    private String instructorName;
+    private String locationName;
+    private String locationAddress;
+    private String createdAt;
+    private String updatedAt;
+
+    // Getters
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public String getDiscipline() { return discipline; }
+    public int getDefaultDurationMin() { return defaultDurationMin; }
+    public int getDefaultCapacity() { return defaultCapacity; }
+    public String getInstructorName() { return instructorName; }
+    public String getLocationName() { return locationName; }
+    public String getLocationAddress() { return locationAddress; }
+    public String getCreatedAt() { return createdAt; }
+    public String getUpdatedAt() { return updatedAt; }
+}
+
